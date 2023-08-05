31af9147152214f775abd245ddb545920657e6e0
serkan <serkanmazlum306@gmail.com>
2023-08-05 17:28:27 +0300

    genel düzenlemler

--- src/camera_recorder/camera_recorder/camera_recorder.py
+++ src/camera_recorder/camera_recorder/camera_recorder.py
@@ -5,15 +5,13 @@ import rclpy
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-from ament_index_python.packages import get_package_share_directory
 
 class Recorder(Node):
   def __init__(self):
     super().__init__('camera_rec')
-    self.subscriptions = self.create_subscription(Image, 'camera', self.callbackCamera, 10)
+    self.sub_ = self.create_subscription(Image, 'camera', self.callbackCamera, 10)
     
-    land_vehicle_path = get_package_share_directory("land_vehicle")
-    video_path = os.path.join(land_vehicle_path, "video/front_camera.avi")
+    video_path = os.path.join(os.getcwd(), "front_camera.avi")
     self.out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc('M','J','P','G'),
                              15, (360, 360))
     self.bridge = CvBridge()
