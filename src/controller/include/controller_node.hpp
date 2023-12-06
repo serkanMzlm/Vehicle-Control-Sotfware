44b6a4c5e81f49c6b1c0c4f9218e28087816dc3e
serkan <serkanmazlum306@gmail.com>
2023-12-06 22:01:57 +0300

    The controller package has been reset to default

--- src/controller/include/controller_node.hpp
+++ src/controller/include/controller_node.hpp
@@ -6,34 +6,11 @@
 #include "geometry_msgs/msg/twist.hpp"
 
 
-
-using int32Msg = std_msgs::msg::Int32;
-using twistMsg = geometry_msgs::msg::Twist;
-
-typedef enum {
-  LINEAR, ANGULAR, L_SCALE, A_SCALE, SPEED_ALL_DATA
-} Joy_e;
-
-typedef union{
-  struct{
-    float linear;
-    float angular;
-    float l_scale;
-    float a_scale;
-  }Joy_s;
-  float data[SPEED_ALL_DATA];
-}Joy_t;
-
 class Controller: public rclcpp::Node{
 public:
   Controller();
-  void joyCallback(int32Msg);
 
 private:
-  Joy_t speed;
-  bool is_ready;
-  rclcpp::Subscription<int32Msg>::SharedPtr keyboard_sub;
-  rclcpp::Publisher<twistMsg>::SharedPtr keyboard_pub;
 };
 
 #endif
--- src/controller/src/controller_node.cpp
+++ src/controller/src/controller_node.cpp
@@ -1,12 +1,10 @@
 #include "controller.hpp"
 using namespace std::placeholders;
 
-Controller::Controller(): Node("controller_node"), is_ready(true){
-  keyboard_pub = this->create_publisher<twistMsg>("cmd_vel", 10);
+Controller::Controller(): Node("controller_node"){
+  // keyboard_pub = this->create_publisher<twistMsg>("cmd_vel", 10);
 }
 
-void Controller::joyCallback(const int32Msg msg){
-}
 
 int main(int argc, char ** args){
   rclcpp::init(argc, args);
