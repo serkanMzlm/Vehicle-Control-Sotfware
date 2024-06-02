ed696cf16683de3e6c0fb84df2bfcca441e32842
serkan <serkanmazlum306@gmail.com>
2024-06-02 15:04:02 +0300

    Added library to calculate time step

---
+++ src/include/time_utility/src/tic_toc.cpp
@@ -0,0 +1,37 @@
+#include "tic_toc.hpp"
+
+TicToc::TicToc()
+{
+    tic();
+}
+
+void TicToc::tic()
+{
+    this->start = std::chrono::steady_clock::now();
+}
+
+double TicToc::toc()
+{
+    end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_sec = end - start;
+    return elapsed_sec.count() * 1000;
+}
+
+uint64_t TicToc::timeStep()
+{
+    struct timespec tms;
+
+    if (!timespec_get(&tms, TIME_UTC))
+    {
+        return 0;
+    }
+
+    uint64_t microsecond = tms.tv_sec * 1000000 + tms.tv_nsec / 1000;
+    if (tms.tv_nsec % 1000 >= 500)
+    {
+        microsecond++;
+    }
+
+    int64_t millisecond = microsecond / 1000;
+    return millisecond;
+}
