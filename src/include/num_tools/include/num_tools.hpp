de65e95f7ecf6ddf49c87be004a8d50efe294270
serkan <serkanmazlum306@gmail.com>
2024-06-08 13:18:09 +0300

    Comments have been added to the num_tools package.

--- src/include/num_tools/include/num_tools.hpp
+++ src/include/num_tools/include/num_tools.hpp
@@ -1,9 +1,27 @@
 #ifndef __NUM_TOOLS_HPP__
 #define __NUM_TOOLS_HPP__
 
-double map(double data, double in_min, double in_max,
-           double out_min, double out_max);
+/**
+ * @brief Maps a value from one range to another.
+ *
+ * @param data The input value to be mapped.
+ * @param in_min The minimum value of the input range.
+ * @param in_max The maximum value of the input range.
+ * @param out_min The minimum value of the output range.
+ * @param out_max The maximum value of the output range.
+ * @return The mapped value in the output range.
+ */
+double mapValue(double data, double in_min, 
+                double in_max, double out_min, double out_max);
 
-float constrain(float value, const float min_value, const float max_value);
+/**
+ * @brief Constrain a value within a specified range.
+ *
+ * @param value The value to be constrained.
+ * @param min_value The minimum allowed value (inclusive).
+ * @param max_value The maximum allowed value (inclusive).
+ * @return The constrained value within the specified range.
+ */
+float constrainValue(float value, const float min_value, const float max_value);
 
 #endif
--- src/include/num_tools/src/num_tools.cpp
+++ src/include/num_tools/src/num_tools.cpp
@@ -1,13 +1,22 @@
 #include "num_tools.hpp"
 #include <cmath>
 
-double map(double data, double in_min, double in_max,
-           double out_min, double out_max)
+double mapValue(double data, double in_min,
+                double in_max, double out_min, double out_max)
 {
-  return ((((data - in_min) * (out_max - out_min)) / (in_max - in_min)) + out_min);
+  if (in_max == in_min)
+  {
+    return out_min;
+  }
+
+  // Calculate the proportion of 'data' within the input range
+  double proportion = (data - in_min) / (in_max - in_min);
+
+  double mapped_value = proportion * (out_max - out_min) + out_min;
+  return mapped_value;
 }
 
-float constrain(float value, const float min_value, const float max_value)
+float constrainValue(float value, const float min_value, const float max_value)
 {
   return fminf(max_value, fmaxf(min_value, value));
 }
