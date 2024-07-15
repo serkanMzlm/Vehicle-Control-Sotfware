2f64c586ad2c8d1711c01bb58c6ca1c437bb8da4
serkan <serkanmazlum306@gmail.com>
2024-07-15 18:13:43 +0300

    urdf edited

--- src/modules/vehicle_control_software/launch/rviz_launch.py
+++ src/modules/vehicle_control_software/launch/rviz_launch.py
@@ -6,35 +6,21 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from os.path import join as Path
 
-
-gz_sim_resource_path = os.environ.get("GZ_SIM_RESOURCE_PATH")
-
-if gz_sim_resource_path:
-    paths = gz_sim_resource_path.split(":")
-    desired_path = None
-
-    for path in paths:
-        if "Vehicle-Control-Sotfware" in path:
-            desired_path = path
-            break
-    
-    if desired_path:
-        directory = desired_path.split("/")
-        sim_world_path = "/".join(directory[:-1])
-        sim_launch_file = simulation_world_path = Path(sim_world_path, "models", "urdf", "vehicle.urdf")
+gz_sim_resource_path = get_package_share_directory('vehicle_control_software')
+urdf_file = os.path.join(gz_sim_resource_path, "urdf", "vehicle.urdf")
 
 
 robot_state_pub = Node(
     package="robot_state_publisher",
     executable="robot_state_publisher",
     output='screen',
-    arguments=[sim_launch_file],
+    arguments=[urdf_file],
 )
 
 joint_state_pub = Node(
     package="joint_state_publisher_gui",
     executable="joint_state_publisher_gui",
-    arguments=[sim_launch_file],
+    arguments=[urdf_file],
 )
 
 def generate_launch_description():
